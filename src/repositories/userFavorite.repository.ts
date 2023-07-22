9ee7481804eee13deecb9077114581e7c8c54384
leeool <lgonsalez02@gmail.com>
2023-07-22 12:33:15 -0300

    🔥 feat: remove user's favorite entity

--- src/repositories/userFavorite.repository.ts
+++
@@ -1,6 +0,0 @@
-import AppDataSource from "@dataSource"
-import UserFavorite from "@entities/UserFavorite"
-
-const userRepository = AppDataSource.getRepository(UserFavorite)
-
-export default userRepository
