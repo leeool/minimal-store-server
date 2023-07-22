a3936e608cc0f659499772cc1e63277335d3e8a1
leeool <lgonsalez02@gmail.com>
2023-07-22 12:33:42 -0300

    ✨ feat: add user's favorite controller

---
+++ src/controllers/userFavorite.controller.ts
@@ -0,0 +1,75 @@
+import productRepository from "@repositories/product.repository"
+import userRepository from "@repositories/user.repository"
+import { Response, Request } from "express"
+import { In } from "typeorm"
+
+class UserFavoriteController {
+  async showByUser(req: Request, res: Response) {
+    const { id } = req.params
+
+    const userFavorites = await userRepository.find({
+      where: { id },
+      relations: { favorites: true }
+    })
+
+    console.log(userFavorites)
+
+    return res.json(userFavorites)
+  }
+
+  async store(req: Request, res: Response) {
+    const { id } = req.params
+    const { productId } = req.body
+
+    const user = await userRepository.findOne({
+      where: { id },
+      relations: { favorites: true }
+    })
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" })
+    }
+
+    const products = await productRepository.findOneBy({ id: productId })
+
+    if (!products) {
+      return res.status(400).json({ message: "Product not found" })
+    }
+
+    await userRepository.save({
+      ...user,
+      favorites: [...user.favorites, products]
+    })
+
+    return res.json(products)
+  }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params
+    const { productId } = req.body
+
+    const user = await userRepository.findOne({
+      where: { id },
+      relations: { favorites: true }
+    })
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" })
+    }
+
+    const products = await productRepository.findOneBy({ id: productId })
+
+    if (!products) {
+      return res.status(400).json({ message: "Product not found" })
+    }
+
+    await userRepository.save({
+      ...user,
+      favorites: user.favorites.filter((favorite) => favorite.id !== productId)
+    })
+
+    return res.sendStatus(204)
+  }
+}
+
+export default new UserFavoriteController()
