7eafa9e45379025290973605a2554b2afd9c7b59
leeool <lgonsalez02@gmail.com>
2023-07-25 00:14:15 -0300

    🐛 fix: connection to database

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -9,6 +9,9 @@ const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
   migrations: [__dirname + "/**/migrations/*.{ts,js}"],
   entities: [__dirname + "/**/entities/*.{ts,js}"]
 })
